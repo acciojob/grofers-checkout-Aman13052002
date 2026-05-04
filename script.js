8a727e6fa58122dd3e5a431b1b68b07e5f858c29
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-05-05 01:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const prices = document.querySelectorAll('.prices');
 const table = document.querySelector('table');
-let totalPrice = 0;
+const button = document.querySelector('button');
 
-prices.forEach((price)=>{
-	totalPrice += Number(price.textContent);
-})
-const newRow = document.createElement('tr');
-newRow.innerHTML = `<td>Total Price</td>
-					<td>${totalPrice}</td>
-`;
-table.appendChild(newRow);
+button.addEventListener('click', function () {
+  let totalPrice = 0;
+
+  prices.forEach((price) => {
+    totalPrice += Number(price.textContent);
+  });
+
+  const existing = document.querySelector('#ans');
+  if (existing) existing.remove();
+
+  const newRow = document.createElement('tr');
+  newRow.id = "ans";   
+
+  newRow.innerHTML = `
+    <td>Total Price</td>
+    <td>${totalPrice}</td>
+  `;
+
+  table.appendChild(newRow);
+});
